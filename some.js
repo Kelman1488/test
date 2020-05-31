c8d6711e00110a6462b688f7cef70e008095ef1b
Kelman <yura.kotenko,9b@gmail.com>
2020-05-31 18:13:26 +0300

    Мой первый коммит

---
+++ some.js
@@ -0,0 +1,22 @@
+"use strict";
+console.log(4/0);
+console.log('string'*9);
+
+let something
+console.log(something);
+
+let person = {
+  name: "John",
+  age: 25,
+  isMarried: false
+};
+
+console.log(person['name']);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[0]);
+
+alert("Hello World!");
+
+console.log("arr" + "=" "- object");
